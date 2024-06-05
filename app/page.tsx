1bb2dd1d908992479413f89f08663cdac7577c37
vikalp <avikalp233@gmail.com>
2024-06-05 20:42:27 +0530

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+
+      <h1>Dashboard</h1>
+
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+
+    </div>
   );
 }
